aa818429c5922a219b26585d139bd83923eecaf2
littlebird <305401660@qq.com>
2016-12-04 13:05:40 +0800

    Create resume.js

---
+++ javascript/resume.js
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="前端，前端工程师简历，web前端，前端简历">
+    <meta name="description" content="周何龙web前端工程师简历">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>周何龙的简历</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/reset.css">
+	<link rel="stylesheet" type="text/css" href="style/jquery.fullPage.min.css">
+	<link rel="stylesheet" type="text/css" href="style/media-queries.css">
+</head>
+<body>
+<div id="mainMenu" class="clearfix">
+    <div class="avatar clearfix">
+        <a href="#page1"><img src="images/logo.gif"></a>
+        <h1>周何龙</h1>
+        <p>web前端学习者</p>
+    </div>
+	<ul class="mainNav">
+	    <li data-menuanchor="page1" class="active"><a href="#page1">DEMO</a></li>
+	    <li data-menuanchor="page2"><a href="#page2">技能</a></li>
+	    <li data-menuanchor="page3"><a href="#page3">经历</a></li>
+	    <li data-menuanchor="page4"><a href="#page4">简介</a></li>
+	</ul>
+	<ul class="contact">
+	    <li><a href="javascript:void(0);" class="weixin">gd0668k</a></li>
+	    <li><a href="javascript:void(0);" class="phone">18682257830</a></li>
+	    <li><a href="javascript:void(0);" class="email">18682257830@qq.com</a></li>
+	</ul>
+</div>
+<div id="content">
+    <div class="section demo">
+	    <h2>前端DEMO</h2>
+	    <div class="clearfix">
+	        <div class="box">
+                <a href=""><img src="images/newegg.gif"></a>
+                <h3>电商网站首页</h3>
+                <p>遵循Dom编程规范，js功能基本实现,页面效果几乎一比一还原</p>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/newegg2.png"></a>
+                <h3>商品详情页</h3>
+                <p>兼容不同浏览器（含ie7以上），实现导航固定网页内容动态切换效果</p>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/weibo.png"></a>
+                <h3>响应式微博首页</h3>
+                <p>百分比浮动布局简单实现手机，平板，电脑不同尺寸设备网页自适应</p>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/bootstrap.png"></a>
+                <h3>bootstrap个人博客</h3>
+                <p>了解bootstrap源码实现原理，理解移动优先理念</p>
+	        </div>
+	    </div>
+	    <div class="clearfix">
+	        <div class="box">
+                <a href=""><img src="images/2048.png"></a>
+                <h3>javascript2048游戏</h3>
+                <p>响应式设计,自适应设备屏幕尺寸，可打包成移动webapp</p>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/calculator.png"></a>
+                <h3>javascript计算器</h3>
+                <p>简单js运算符操作实现计算器，可打包成webapp</p>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/weibo.png"></a>
+                <h3>在线视频弹幕效果</h3>
+	        </div>
+	        <div class="box">
+                <a href=""><img src="images/Pomodoro.png"></a>
+                <h3>个性化番茄钟</h3>
+                <p>css3和jQuery实现的圆环倒计时效果，jQuery实现可添加删除的任务清单</p>
+	        </div>
+	    </div>
+	</div>
+	<div class="section skill">
+	    <h2>专业技能</h2>
+	    <div class="clearfix">
+	        <div class="box">
+               <h3>HTML&amp;CSS</h3>
+               <ul>
+                   <li>了解W3C规范，可以编写语义化的HTML,模块化的CSS,完成常见的自适应、响应式网页布局</li>
+                   <li>熟悉常见过渡，3d，变化，移动等css3新特性</li>
+                   <li>了解html5标签属性，理解移动优先概念</li>
+                   <li>编写代码遵循样式结构分离原则</li>
+               </ul>
+	        </div>
+	        <div class="box">
+               <h3>javascript</h3>
+               <ul>
+                   <li>深入理解js原型和闭包</li>
+                   <li>可使用原生js进行开发</li>
+                   <li>了解面向对象的js，了解DOM编程及浏览器兼容问题</li>
+                   <li>了解ajax的获取和使用，可以用js编写ajax函数</li>
+               </ul>
+	        </div>
+	        <div class="box">
+               <h3>jQuery</h3>
+               <ul>
+                   <li>熟悉使用jQuery的事件和动画实现页面效果和交互</li>
+                   <li>熟悉使用fullpage、move、fly、Validation等常用jQuery插件</li>
+                   <li>了解jQuery插件的封装</li>
+               </ul>
+	        </div>
+	        <div class="box">
+                <h3>bootstrap</h3>
+                <ul>
+                   <li>熟悉使用bootstrap构建网页</li>
+                   <li>理解bootstrap源码实现原理</li>
+                   <li>可以定制个性化bootstrap</li>
+               </ul>
+	        </div>
+	    </div>
+	</div>
+	<div class="section experience">
+	    <h2>工作经历</h2>
+	    <div class="clearfix info">
+		    <div class="slide"><img src="images/1.jpg"></div>
+	        <div class="slide"><img src="images/2.jpg"></div>
+	        <div class="slide"><img src="images/3.jpg"></div>
+	    </div>
+	    <p>以往的工作经历让我明白到每个岗位都十分重要，工作中要全力以赴做到最好。</p>
+	</div>
+	<div class="section about">
+	    <h2>关于我</h2>
+	    <div class="clearfix">
+		    <div class="section2">
+	            <a href=""><img src="images/personal.png"></a>
+	            <h3>基本信息</h3>
+                <p>姓名：周何龙</p>
+                <p>年龄：26岁</p>
+                <p>籍贯：广东茂名</p>
+		    </div>
+		    <div class="section2">
+	            <a href=""><img src="images/education.png"></a>
+	            <h3>毕业院校</h3>
+                <p>中国民航大学</p>
+		    </div>
+		    <div class="section2">
+	            <a href=""><img src="images/sign.png"></a>
+	            <h3>目前坐标</h3>
+	            <p>深圳市福田区上梅林</p>
+		    </div>
+		</div>
+		<p>我重视承诺和责任，能在约定时间内保证完成任务。<br>
+	    有良好的编码习惯，较强的自学能力和良好的学习习惯，对新知识充满好奇心。<br>
+	    半年前端开发经验，熟悉HTML、CSS、Javascript，能熟练开发符合W3C标准的页面，熟练使用jQuery框架和主流jQuery插件。
+	   </p>
+	</div>
+</div>
+
+<script type="text/javascript" src="javascript/jquery-3.1.1.slim.min.js"></script>
+<script type="text/javascript" src="javascript/jquery.fullPage.min.js"></script>
+<script type="text/javascript" src="javascript/resume.js"></script>
+<script type="text/javascript" src="javascript/move.js"></script>
+</body>
+</html>
